415dc7417cd29c9c16ecd0b1a1302f55482929dc
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-02-18 22:54:48 +0900

    Update index.html, main.js  - スタートまでの流れ

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import {fileLoad, progressbarSet} from "./databaseLoader";
 import {keyboardGen} from "./keyboard";
+import {sleep} from "./commonFunc";
+const $ = require("jquery");
 
 document.addEventListener("DOMContentLoaded", ()=>{
 	progressbarSet(0);
 	fileLoad();
 	keyboardGen();
+
+	const pressStart = async e=>{
+		if(!$(".container#start").hasClass("showing")) return;
+		if(e.keyCode === 32){  // Space
+			window.removeEventListener("keydown", pressStart);
+			const $e = $(".container#start p");
+			$e.text("3");
+			await sleep(1000);
+			$e.text("2");
+			await sleep(1000);
+			$e.text("1");
+			await sleep(1000);
+			$e.text("Start!");
+			await sleep(1000);
+			$(".container#start").removeClass("showing");
+			$(".container#main").addClass("showing");
+		}
+	}
+	window.addEventListener("keydown", pressStart);
 });
