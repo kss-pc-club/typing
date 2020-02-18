8d83360a83a453380b286f95db5bde743a9b6eb7
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-02-18 22:41:37 +0900

    Update keyboard.js, style.scss  - Add Keyevent Listener

---
+++ src/keyboard.js
@@ -0,0 +1,20 @@
+const $ = require("jquery")
+
+const keyboardGen = ()=>{
+	for(let i=10; i<36; i++){
+		const key = document.createElement("div");
+		key.classList.add("key");
+		key.innerText = i.toString(36).toUpperCase();
+		key.style.gridArea = i.toString(36).toLowerCase();
+		key.id = i.toString(36).toLowerCase();
+		document.querySelector(".container#main .keyboard").appendChild(key);
+	}
+}
+
+const code2char = c=>(Number(c) - 55).toString(36);
+
+
+window.addEventListener("keydown", e=>{	if(e.keyCode >= 65 && e.keyCode <= 90){ $(`.key#${code2char(e.keyCode)}`).addClass("pressing"); } });
+window.addEventListener("keyup", e=>{ if(e.keyCode >= 65 && e.keyCode <= 90){ $(`.key#${code2char(e.keyCode)}`).removeClass("pressing"); } });
+
+export {keyboardGen, code2char}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import {fileLoad, progressbarSet} from "./databaseLoader";
+import {keyboardGen} from "./keyboard";
 
 document.addEventListener("DOMContentLoaded", ()=>{
 	progressbarSet(0);
 	fileLoad();
+	keyboardGen();
 });
