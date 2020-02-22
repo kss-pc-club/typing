0696e5a10595d3b8d6a2e6ceaccb37bfadb11cf5
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-02-22 17:40:00 +0900

    Update score.js  - Change how to calc score

--- src/score.js
+++ src/score.js
@@ -3,7 +3,7 @@ const scoreShow = data=>{
 	const incorrect = data.incorrectType;
 	const totalType = correct + incorrect;
 	const correctRate = Math.round((correct / totalType) * 10000) / 100;
-	const score = Math.round(correct**2 / totalType * 10000) / 100;
+	const score = Math.round(correct**2 / totalType * 100);
 	console.log(`Correct Type: ${correct}\nIncorrect Type: ${incorrect}\nTotal: ${totalType}\nCorrect Rate: ${correctRate}\nScore: ${score}`)
 };
 
